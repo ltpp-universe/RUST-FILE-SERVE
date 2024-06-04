3a55d43217e0d1f536080058cadf986538c9606a
尤雨东 <root@ltpp.vip>
2024-06-04 21:28:31 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/rust-file-serve.git;
-git remote add github git@github.com:wmzn-ltpp/RUST-FILE-SERVE.git;
+git remote add github git@github.com:ltpp-universe/RUST-FILE-SERVE.git;
