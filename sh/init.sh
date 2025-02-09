d41d87dd4f20a553f588370bd8e461a3f8946896
尤雨东 <root@ltpp.vip>
2025-02-09 20:57:03 +0800

    feat:sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git init;
 git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/rust-file-serve.git;
+git remote add gitee git@gitee.com:ltpp-universe/RUST-FILE-SERVE.git;
 git remote add origin git@github.com:ltpp-universe/RUST-FILE-SERVE.git;
